0428e9b2feeca4757292f7f4581cda6cc2dd9e70
Nicolas Fraenkel <nicfraenkel@gmail.com>
2022-12-08 21:58:39 +1030

    quick commit for review merge

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-const { Posts, User, Comment } = require('../models');
+const { Posts, Users, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 // GET all posts for homepage
 router.get('/', async (req, res) => {
     try {
-        const postData = await Post.findAll({
+        const postData = await Posts.findAll({
         });
     
         const posts = postData.map((post) => post.get({ plain: true }));
