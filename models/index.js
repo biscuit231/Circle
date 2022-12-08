daba7731bdfec6ace90d6cdec1a6d0f2ae0c9f62
Jesse <jesse_11biscuit@hotmail.com>
2022-12-08 21:58:04 +1030

    base model structure

---
+++ models/index.js
@@ -0,0 +1,57 @@
+//import models
+const Users = require('./Users');
+const Communities = require('./Communities');
+const Posts = require('./Posts');
+const Replys = require('./Replys');
+const Files = require('./Files');
+const Friends = require('./Friends');
+
+
+Posts.belongsTo(Communities, {
+  foreignKey: 'Communities_id'
+});
+Communities.hasMany(Posts, {
+  foreignKey: ''
+});
+
+
+Replys.belongsTo(Posts, {
+  foreignKey: 'post_replys'
+});
+Posts.hasMany(Replys, {
+  foreignKey: 'replys_id'
+});
+
+
+Posts.belongsTo(Users, {
+  foreignKey: ''
+});
+Users.hasMany(Posts, {
+  foreignKey: ''
+});
+
+
+Replys.belongsTo(Users, {
+  foreignKey: ''
+});
+Users.hasMany(Replys, {
+  foreignKey: ''
+});
+
+
+Communities.hasOne(Users, {
+  foreignKey: ''
+});
+Users.hasMany(Communities, {
+  foreignKey: ''
+});
+
+
+module.exports = {
+    Users,
+    Communities,
+    Posts,
+    Replys,
+    Files,
+    Friends,
+  };
