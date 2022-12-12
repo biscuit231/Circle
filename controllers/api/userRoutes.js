00d95f43c310a47e3b2dfa135925e6cf45d98663
Nicolas Fraenkel <nicfraenkel@gmail.com>
2022-12-12 16:54:09 +1030

    Added to userRoutes
    Create new user Route
    Login Route
    Logout Route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const { Users } = require('../../models');
 
-
-
 //get all users
 router.get('/users', async (req, res) => {
     try {
@@ -36,3 +34,49 @@ router.post('/signup', async (req, res) => {
     } 
 })
 
+//login
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await Users.findOne({ where: { email: req.body.email } });
+
+        if (!userData) {
+            res 
+                .status(400)
+                .json({ message: 'Incorrect email or password, please try again' });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+        
+        if (!validPassword) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password, please try again' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            TODO://add user id to session, need to check if i can use user_id from model?
+            res 
+                .status(200)
+                .json({ user: userData, message: 'You are now logged in!' });
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) { 
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+
+module.exports = router;
