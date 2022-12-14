0984ca45b65ae26ebf090a160b1386b64dac518b
Nicolas Fraenkel <nicfraenkel@gmail.com>
2022-12-14 14:01:17 +1030

    Updated seed files:
    You now only need to run NPM RUN SEED_USERS

--- seeds/seed_users.js
+++ seeds/seed_users.js
@@ -1,15 +1,31 @@
 const sequelize = require('../config/connection');
-const { Users } = require('../models');
+const { Users, Posts, Replys } = require('../models');
 
 const userData = require('./userData.json');
+const postData = require('./postData.json');
+const replyData = require('./replyData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   await Users.bulkCreate(userData, {
     individualHooks: true,
-    returning: true,
+    returning: true
+
   });
+
+  await Posts.bulkCreate(postData, {
+    individualHooks: true,
+    returning: true
+  });
+
+  await Replys.bulkCreate(replyData, {
+    individualHooks: true,
+    returning: true
+  });
+
+
+  
 
   process.exit(0);
 };
